5b14d77fa5e4b83ce3c53f6f859be8d9dee45f0f
Hoodrabbit <micyle1022@naver.com>
2024-04-03 23:59:05 +0900

    사과 이미지 수정, 이펙트 스크립트 수정, 일시정지 메인메뉴 추가

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -7,10 +7,28 @@ using UnityEngine.UI;
 public class UIManager : Singleton<UIManager>
 {
     public TextMeshProUGUI Score;
+    public GameObject MainMenu;
+    public Button Retry;
+    public Button Quit;
 
 
     public void Update()
     {
         Score.text =  "현재 점수" + GameManager.Instance.Score.ToString();
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
+
+
+
+
+    public void Game_Quit()
+    {
+        Application.Quit();
+    }
+
 }
